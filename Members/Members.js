e61b4245416ea7608de6e05541ca2e218c0ca7fe
M.Uzair <45979852+UzairSamad@users.noreply.github.com>
2019-10-25 01:07:10 +0500

    Dummy Members for Api testing

---
+++ Members/Members.js
@@ -0,0 +1,19 @@
+const members = [
+  {
+    id: 1,
+    name: " Jhon",
+    email: "Jhon.123@gmail.com"
+  },
+  {
+    id: 2,
+    name: " Farukkh",
+    email: "frk_naf.435@gmail.com"
+  },
+  {
+    id: 3,
+    name: " Ali",
+    email: "ali.12@gmail.com"
+  }
+];
+
+module.exports = members;
